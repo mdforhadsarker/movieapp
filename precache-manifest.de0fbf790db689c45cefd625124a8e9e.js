fc14cf9f8716306ff8d6eaec02c27d5d5718bffe
Md Forhad Sarkar <news9097@gmail.com>
2020-07-23 20:25:17 +0800

    Updates

---
+++ precache-manifest.de0fbf790db689c45cefd625124a8e9e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1b0174f7ed88281278c306d0605df84",
+    "url": "/movieapp/index.html"
+  },
+  {
+    "revision": "103afdd9169672d31c33",
+    "url": "/movieapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "19cb7773aa667d261e0b",
+    "url": "/movieapp/static/js/2.cd52c449.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/movieapp/static/js/2.cd52c449.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "103afdd9169672d31c33",
+    "url": "/movieapp/static/js/main.bc2b4d18.chunk.js"
+  },
+  {
+    "revision": "0d516f28c241e3c70098",
+    "url": "/movieapp/static/js/runtime-main.04b21085.js"
+  },
+  {
+    "revision": "3b97bcddd2dab735725ab33f89b64b75",
+    "url": "/movieapp/static/media/Spinner.3b97bcdd.gif"
+  },
+  {
+    "revision": "e29de0f86d4e91ac6338dd67d03f4bfb",
+    "url": "/movieapp/static/media/logo.e29de0f8.png"
+  }
+]);
